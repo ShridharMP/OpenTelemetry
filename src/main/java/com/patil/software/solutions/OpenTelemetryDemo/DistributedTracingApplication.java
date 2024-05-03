69a416f16abc90d5bb576756a047f598ae9b97c0
ShridharMP <mpshridhar2016@gmail.com>
2024-05-03 22:31:13 +0530

    commit

---
+++ src/main/java/com/patil/software/solutions/OpenTelemetryDemo/DistributedTracingApplication.java
@@ -0,0 +1,20 @@
+package com.patil.software.solutions.OpenTelemetryDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DistributedTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTracingApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+}
---
+++ src/test/java/com/patil/software/solutions/OpenTelemetryDemo/OpenTelemetryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patil.software.solutions.OpenTelemetryDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTelemetryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
